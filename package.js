20ec0bdaccc3b483b6ef9e454204f0af07902e3d
djhi <gildas.garcia@gmail.com>
2014-08-28 21:09:21 +0200

    corrected reference to moment package for meteor 0.9.0

--- package.js
+++ package.js
@@ -8,14 +8,14 @@ Package.describe({
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
   
-  api.use(['moment'], where);
-  api.add_files('lib/moment/min/locales.js', where);
+  api.use(['mrt:moment'], where);
+  api.add_files('lib/moment/min/locales.js', 'client');
 });
 
 if (Package.on_test) {
   Package.on_test(function (api) {
     if (Package.onTest) {
-      api.use(['mrt:moment@2.8.1', 'tinytest', 'test-helpers'], ['client', 'server']);
+      api.use(['mrt:moment', 'tinytest', 'test-helpers'], ['client', 'server']);
     } else {
       api.use(['moment', 'tinytest', 'test-helpers'], ['client', 'server']);
     }
