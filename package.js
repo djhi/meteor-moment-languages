7da40ccfddd653f4958cfec7aedbd3e589cb2794
djhi <gildas.garcia@gmail.com>
2014-08-29 20:26:04 +0200

    package.js for meteor 0.9.0

--- package.js
+++ package.js
@@ -8,16 +8,15 @@ Package.describe({
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
   
-  api.use(['mrt:moment'], where);
   api.add_files('lib/moment/min/locales.js', 'client');
 });
 
 if (Package.on_test) {
   Package.on_test(function (api) {
     if (Package.onTest) {
-      api.use(['mrt:moment', 'tinytest', 'test-helpers'], ['client', 'server']);
+      api.use([ 'tinytest', 'test-helpers'], ['client', 'server']);
     } else {
-      api.use(['moment', 'tinytest', 'test-helpers'], ['client', 'server']);
+      api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     }
   });
 }
