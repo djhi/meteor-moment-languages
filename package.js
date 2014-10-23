82069b3bdc3de16e2e6f7d1bdc08e2f38425d2ce
djhi <gildas.garcia@gmail.com>
2014-10-23 11:24:47 +0200

    reverted to client only locales

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "gildaspk:moment-languages",
   summary: "Languages files for Moment.js. See http://momentjs.com.",
-  version: "2.8.3",
+  version: "2.8.4",
   git: "https://github.com/djhi/meteor-moment-languages.git",
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('lib/moment/min/locales.js', ['client', 'server']);
+  api.add_files('lib/moment/min/locales.js', ['client']);
 });
 
 if (Package.on_test) {
